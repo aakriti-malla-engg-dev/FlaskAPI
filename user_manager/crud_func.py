e6d8b9da44eb3680860344c3bff72fec9b1815c6
aakriti-malla-engg-dev <aakriti.malla@moengage.com>
2023-03-13 15:34:22 +0530

    Adding try/catch

--- user_manager/crud_func.py
+++ user_manager/crud_func.py
@@ -1,12 +1,11 @@
 import bson
+import pymongo
 from pymongo import MongoClient
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 import json
 from bson.json_util import dumps
 
 from bson import json_util, ObjectId
-from user_func import Userschema, User
-from UserClass import UserSchema, UserDetails
 
 conn = 'mongodb://127.0.0.1:27017/Users'
 client = MongoClient(conn)
@@ -29,111 +28,157 @@ def menu():
 
 @app.route('/users', methods=['POST'])
 def add_user_to_db():
-    data = request.json
-    user_name = data['name']
-    mobile_no = data['mobile_no']
-    city = data['city']
-    user_id = None
-    if user_name and mobile_no and city:
-        if collection_name.find_one({"mobile_no": data['mobile_no']}):
-            user = collection_name.find_one({"mobile_no": data['mobile_no']})
-            user_id = str(user['_id'])
-            return jsonify({
-                "status": 200,
-                "message": "User Already Exists!",
-                "id": user_id
-            })
-        else:
-            status = collection_name.insert_one({
-                "name": user_name,
-                "mobile_no": mobile_no,
-                "city": city
-            })
-            user_id = str(status.inserted_id)
-            return jsonify({
-                "status": 201,
-                "message": "User Created!",
-                "data": {
+    try:
+        data = request.json
+        user_name = data['name']
+        mobile_no = data['mobile_no']
+        city = data['city']
+        if user_name and mobile_no and city:
+            if collection_name.find_one({"mobile_no": data['mobile_no']}):
+                user = collection_name.find_one({"mobile_no": data['mobile_no']})
+                user_id = str(user['_id'])
+                return jsonify({
+                    "status": 200,
+                    "message": "User Already Exists!",
                     "id": user_id
-                }
+                })
+            else:
+                status = collection_name.insert_one({
+                    "name": user_name,
+                    "mobile_no": mobile_no,
+                    "city": city
+                })
+                user_id = str(status.inserted_id)
+                return jsonify({
+                    "status": 201,
+                    "message": "User Created!",
+                    "data": {
+                        "id": user_id
+                    }
+                })
+        else:
+            return jsonify({
+                "status": 400,
+                "message": "Invalid Request Data"
             })
-    else:
+    except Exception as e:
         return jsonify({
-            "status": 400,
-            "message": "Invalid Request Data"
+            'Error': str(e),
+            'status': 404
         })
 
 
 @app.route('/users/<mobile_no>', methods=['GET'])
 def get_user_from_db(mobile_no):
-    user = collection_name.find_one({'mobile_no': mobile_no})
-    if user:
-        result = {}
-        for key, val in user.items():
-            if isinstance(val, ObjectId):
-                result[key] = str(val)
-            else:
-                result[key] = val
+    try:
+        user = collection_name.find_one({'mobile_no': mobile_no})
+        if user:
+            result = {}
+            for key, val in user.items():
+                if isinstance(val, ObjectId):
+                    result[key] = str(val)
+                else:
+                    result[key] = val
+            return jsonify({
+                "status": 200,
+                'data': result,
+                'message': 'User Found!'
+            })
+        else:
+            return jsonify({
+                'status': 404,
+                'message': 'User not found!'
+            })
+    except pymongo.errors.ConnectionFailure:
+        print("Connection Error!")
+    except pymongo.errors.OperationFailure as e:
+        print(f"MongoDB operation failed with error: {e}")
+    except Exception as e:
         return jsonify({
-            "status": 200,
-            'data': result,
-            'message': 'User Found!'
-        })
-    else:
-        return jsonify({
-            'status': 404,
-            'message': 'User not found!'
+            'status': 500,
+            'Message': 'Internal Server Error: ' + str(e)
         })
 
 
 @app.route('/users', methods=['GET'])
 def get_users_from_db():
-    users = []
-    for doc in collection_name.find():
-        users.append(json_util.dumps(doc))
+    try:
+        users = []
+        for doc in collection_name.find():
+            users.append(json_util.dumps(doc))
 
-    output = []
-    for my_dict in users:
-        dict_obj = json.loads(my_dict)
-        output.append(dict_obj)
+        output = []
+        for my_dict in users:
+            dict_obj = json.loads(my_dict)
+            output.append(dict_obj)
 
-    return jsonify({
-        "status": 200,
-        "data": output,
-        "message": "Users Found!"
-    })
+        return jsonify({
+            "status": 200,
+            "data": output,
+            "message": "Users Found!"
+        })
+    except pymongo.errors.ConnectionFailure:
+        print("Connection Error!")
+    except pymongo.errors.OperationFailure as e:
+        print(f"MongoDB operation failed with error: {e}")
+    except Exception as e:
+        return jsonify({
+            'status': 500,
+            'Message': 'Internal Server Error: ' + str(e)
+        })
 
 
 @app.route('/users/<mobile_no>', methods=['PUT'])
 def update_user_in_db(mobile_no):
     updated_details = request.get_json()
     user = collection_name.find_one({'mobile_no': mobile_no})
-    if user:
-        collection_name.update_one({"mobile_no": mobile_no}, {"$set": updated_details})
+    try:
+        if user:
+            collection_name.update_one({"mobile_no": mobile_no}, {"$set": updated_details})
+            return jsonify({
+                'Message': 'SUCCESS',
+                'status': 200
+            })
+        else:
+            return jsonify({
+                'status': 404,
+                'Message': 'User Not Found!'
+            })
+    except pymongo.errors.ConnectionFailure:
+        print("Connection Error!")
+    except pymongo.errors.OperationFailure as e:
+        print(f"MongoDB operation failed with error: {e}")
+    except Exception as e:
         return jsonify({
-            'Message': 'SUCCESS',
-            'status': 200
-        })
-    else:
-        return jsonify({
-            'status': 404,
-            'Message': 'User Not Found!'
+            'status': 500,
+            'Message': 'Internal Server Error: ' + str(e)
         })
 
 
-@app.route('/users/<mobile_no>', methods=['DELETE'])
+@app.route('/users/<int:mobile_no>', methods=['DELETE'])
 def delete_users_from_db(mobile_no):
-    deleted = collection_name.delete_one({'mobile_no': mobile_no})
-    if deleted.deleted_count == 1:
-        return jsonify({
-            'status': 204,
-            'message': 'User Deleted!'
-        })
+    try:
+        deleted = collection_name.delete_one({'mobile_no': mobile_no})
+        if deleted.deleted_count == 1:
+            return jsonify({
+                'status': 204,
+                'message': 'User Deleted!'
+            })
 
-    else:
+        else:
+            return jsonify({
+                'status': 404,
+                'message': 'User Not Found!'
+            })
+    except pymongo.errors.ConnectionFailure:
+        print("Connection Error!")
+    except pymongo.errors.OperationFailure as e:
+        print(f"MongoDB operation failed with error: {e}")
+    except Exception as e:
         return jsonify({
-            'status': 404,
-            'message': 'User Not Found!'
+            'status': 500,
+            'message': 'Error deleting user',
+            'error': str(e)
         })
 
 
